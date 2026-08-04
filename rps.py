385aae00d356df14ae9d0701d103835b6aab5a9c
Abhirama MB <contact.abhimb@gmail.com>
2026-08-05 00:31:55 +0530

    add rps.py

---
+++ rps.py
@@ -0,0 +1,41 @@
+import random as r
+
+print("\nWelcome to ROCK, PAPER, SCISSORS game \n")
+choices = ('r','p','s')
+
+while True:
+    my_choice=input("Enter (r/p/s) : ").lower()
+    if my_choice not in choices:
+        print("Invalid Choice")
+        continue
+        
+    computer_choice=r.choice(choices)
+    print(f"Computer chose: {computer_choice}")
+
+    if my_choice == 'r' :
+        if computer_choice == 's':
+            print("you won")
+        elif computer_choice == 'p':
+            print("you lost")
+        else:
+            print("Tie")
+
+    elif my_choice == 'p':
+        if computer_choice == 'r':
+            print("you won")
+        elif computer_choice == 's':      
+            print("you lost")
+        else:
+            print("Tie")
+
+    elif my_choice == 's':
+        if computer_choice == 'p':
+            print("you won")
+        elif computer_choice == 'r':      
+            print("you lost")
+        else:
+            print("Tie")
+
+    
+
+
