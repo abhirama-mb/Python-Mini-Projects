c7f1d28d1e28bf4a3b36dd024bd659da22d300b3
Abhirama MB <contact.abhimb@gmail.com>
2026-08-18 22:51:12 +0530

    Refractoring number-guessing-game.py

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -1,35 +1,60 @@
-from random import randint 
+from random import randint
 
-print("\nGuess the Number from 1 to 1000\n")
 
-random_num = randint(1,1000)
-count_times=0
+def get_guess():
+    while True:
+        try:
+            guess = int(input("Guess the Number : "))
 
-while True:
-    try:
-        guess=int(input("Guess the Number : "))
-    except ValueError:
-        print("Invalid input! Please enter an integer.")
-        continue
+            if 1 <= guess <= 1000:
+                return guess
 
-    count_times += 1
+            print("Please enter a number between 1 and 1000.")
 
-    if not 1 <= guess <= 1000:
-        print("Please enter a number between 1 and 1000.")
-        continue
+        except ValueError:
+            print("Invalid input! Please enter an integer.")
 
+
+def check_guess(guess, random_num):
     if guess == random_num:
+        return "correct"
+    elif guess > random_num:
+        return "high"
+    else:
+        return "low"
+
+
+def display_result(guess, result):
+    if result == "correct":
         print("\nCongratulations!")
         print(f"You guessed {guess} correctly.")
-        print(f"It took you {count_times} attempts.")
-        if count_times <= 10 :
-            print("Impressive! You probably used a Binary Search approach.")
-        print("Thank you!\n")
-        break
-
-    elif guess > random_num:
+    elif result == "high":
         print("High !! Try again")
-
-    else :
+    else:
         print("Low !! Try again")
 
+
+def play_game():
+    print("\nGuess the Number from 1 to 1000\n")
+
+    random_num = randint(1, 1000)
+    count_times = 0
+
+    while True:
+        guess = get_guess()
+        count_times += 1
+
+        result = check_guess(guess, random_num)
+        display_result(guess, result)
+
+        if result == "correct":
+            print(f"It took you {count_times} attempts.")
+
+            if count_times <= 10:
+                print("Impressive! You probably used a Binary Search approach.")
+
+            print("Thank you!\n")
+            break
+
+
+play_game()
