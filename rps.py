c51ff254932e25973fb58389ca217ed87f57ff26
Abhirama MB <contact.abhimb@gmail.com>
2026-08-18 22:21:04 +0530

    Use Functions to add modulations

--- rps.py
+++ rps.py
@@ -1,32 +1,18 @@
 import random as r
 
-print("\nWelcome to ROCK, PAPER, SCISSORS game \n")
-emojis = { 
-    'r':'🪨',
-    'p':'📰',
-    's':'✂️' 
-}
+def get_user_info():
+    while True:
+        user_choice=input("Enter (r/p/s) : ").lower()
+        if user_choice in choices:
+            return user_choice
+        else:
+            print("Invalid Choice")
 
-choices = ('r','p','s')
-
-wins_against = {
-    'r': 's',   
-    'p': 'r', 
-    's': 'p'    
-}
-
-
-while True:
-    my_choice=input("Enter (r/p/s) : ").lower()
-    if my_choice not in choices:
-        print("Invalid Choice")
-        continue
-        
-    computer_choice = r.choice(choices)
-
+def display_choices(my_choice,computer_choice):
     print(f"You chose: {emojis[my_choice]}")
     print(f"Computer chose: {emojis[computer_choice]}")
 
+def determine_winner(my_choice,computer_choice):
     if my_choice == computer_choice:
         print("Its a Tie")
     elif wins_against[my_choice] == computer_choice:
@@ -35,8 +21,31 @@ while True:
         print("You lost ")
 
 
-    if input("Press N to stop (Enter to continue): ").lower() == 'n':
-        break
+def play_rps():
+    print("\nWelcome to ROCK, PAPER, SCISSORS game \n")
+    while True:
+        my_choice = get_user_info()
+
+        computer_choice = r.choice(choices)
+
+        display_choices(my_choice,computer_choice)
+        determine_winner(my_choice,computer_choice)
     
+        if input("Press N to stop (Enter to continue): ").lower() == 'n':
+            break
+    
+choices = ('r','p','s')
 
+emojis = { 
+    'r':'🪨',
+    'p':'📰',
+    's':'✂️' 
+}
 
+wins_against = {
+    'r': 's',   
+    'p': 'r', 
+    's': 'p'    
+}
+
+play_rps()
